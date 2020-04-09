53cf02678f0b9af4319d1aa79514beecc9c24c8d
Leonardo Kameya <leonardo.kameya@gmail.com>
2020-04-09 00:08:22 -0400

    Publushing build

--- Dockerfile
+++ Dockerfile
@@ -1,13 +1,3 @@
-# Stage 1 - the build process
-FROM node:7.10 as build-deps
-WORKDIR /usr/src/app
-COPY package.json ./
-RUN npm install
-COPY . ./
-RUN npm run build
-
-# Stage 2 - the production environment
-FROM nginx:1.12-alpine
-COPY --from=build-deps /usr/src/app/build /usr/share/nginx/html
-EXPOSE 80
-CMD ["nginx", "-g", "daemon off;"]
+FROM socialengine/nginx-spa:latest
+COPY ./build /app 
+RUN chmod -R 777 /app
---
+++ build/precache-manifest.ae716c2109199fc42fa1f74dfb8782ad.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7a519232b0d07550b64e40eeebb2f75c",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7f5743a723c2f1f0e3b4",
+    "url": "/static/css/main.0b85bc00.chunk.css"
+  },
+  {
+    "revision": "780c2b3b96db2eb95172",
+    "url": "/static/js/2.4e2bd464.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.4e2bd464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7f5743a723c2f1f0e3b4",
+    "url": "/static/js/main.38e2fb80.chunk.js"
+  },
+  {
+    "revision": "dd05f9b019343fa06e10",
+    "url": "/static/js/runtime-main.51344e82.js"
+  }
+]);
